7c943f6d8d122dccd2fbc32a3e84b3fda7a15edf
Varusnoya <yiran.feng.jp@gmail.com>
2025-09-03 00:28:46 +0900

    Add Main.java with brand list

--- src/main/java/com/lenshub/Main.java
+++ src/main/java/com/lenshub/Main.java
@@ -2,7 +2,7 @@ package com.lenshub;
 
 public class Main {
     public static void main(String[] args) {
-        String[] brands = {"Canon", "Nikon", "Sony", "Fujifilm"};
+        String[] brands = {"Canon", "Nikon", "Sony"};
         System.out.println("LensHub 镜头品牌列表：");
         for (String brand : brands) {
             System.out.println(brand);
